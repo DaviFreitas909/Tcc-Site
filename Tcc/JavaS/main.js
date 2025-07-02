46ae10bed7565903d2d4aac2f3a6d455462de91e
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-07-02 15:59:07 -0300

    Update main.js

--- Tcc/JavaS/main.js
+++ Tcc/JavaS/main.js
@@ -1,36 +1,36 @@
 const noticias = [
     {
-        imagem: "../../Imagens/noticia1.jpg", // Corrigido para subir dois níveis
+        imagem: "../Imagens/noticia1.jpg", // Corrigido para subir dois níveis
         titulo: "Mães Solo: Juntos Podemos Construir um Lar Digno",
         descricao: "Saiba como ajudar mães solteiras a conquistarem uma moradia digna",
         link: "../Html/Noticias/Noticia1.html" // Corrigido caminho relativo
     },
     {
-        imagem: "../../Imagens/noticia2.png",
+        imagem: "../Imagens/noticia2.png",
         titulo: "LGBTQIA+Moradia:",
         descricao: "Todes merecem um lar digno para viver",
         link: "../Html/Noticias/Noticia2.html"
     },
     {
-        imagem: "../../Imagens/noticia3.jpg",
+        imagem: "../Imagens/noticia3.jpg",
         titulo: "Onde mora a esperança:",
         descricao: "Exposiçao convida a refletir sobre a potencia transformadora da moradia digna",
         link: "../Html/Noticias/Noticia3.html"
     },
     {
-        imagem: "../../Imagens/noticia4.png",
+        imagem: "../Imagens/noticia4.png",
         titulo: "Habitat para a Humanidade revela: Moradia digna transforma saúde das mulheres em comunidades carentes",
         descricao: "Novo relatório da Habitat para a Humanidade vincula melhorias habitacionais em comunidades e favelas urbanas a benefícios importantes para a saúde das mulheres",
         link: "../Html/Noticias/Noticia4.html"
     },
     {
-        imagem: "../../Imagens/noticia5.jpg", // Removido espaço no nome do arquivo
+        imagem: "../Imagens/noticia5.jpg", // Removido espaço no nome do arquivo
         titulo: "Serviços de consultoria pedagógica especializada",
         descricao: "Contratação de consultoria pedagógica para oficinas de saneamento em 33 escolas do Marajó, com capacitação de educadores e materiais didáticos.",
         link: "../Html/Noticias/Noticia5.html"
     },
     {
-        imagem: "../../Imagens/noticia6.jpg",
+        imagem: "../Imagens/noticia6.jpg",
         titulo: "Reconstruindo Vidas: A Restauração de Moradias no RS Pós-Tragédia Climática",
         descricao: "A restauração de moradias no Rio Grande do Sul após enchentes e tragédia climática",
         link: "../Html/Noticias/Noticia6.html"
