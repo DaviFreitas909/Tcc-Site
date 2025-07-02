4f5e6ed80e985d73b5d60f151ba83e547a1839cf
Davi Freitas <davifreitasdebrito909@gmail.com>
2025-07-02 15:42:20 -0300

    Update main.js

--- Tcc/JavaS/main.js
+++ Tcc/JavaS/main.js
@@ -1,40 +1,39 @@
-// Dados das notícias (agora com propriedade 'link' adicionada)
 const noticias = [
     {
-        imagem: "../Imagens/noticia1.jpg",
+        imagem: "../../Imagens/noticia1.jpg", // Corrigido para subir dois níveis
         titulo: "Mães Solo: Juntos Podemos Construir um Lar Digno",
         descricao: "Saiba como ajudar mães solteiras a conquistarem uma moradia digna",
-        link: "..//Html/Noticias/Noticia1.html" // Link adicionado
+        link: "../Html/Noticias/Noticia1.html" // Corrigido caminho relativo
     },
     {
-        imagem: "../Imagens/noticia2.png",
+        imagem: "../../Imagens/noticia2.png",
         titulo: "LGBTQIA+Moradia:",
         descricao: "Todes merecem um lar digno para viver",
-        link: "../../Html/Noticias/Noticia2.html" // Link adicionado
+        link: "../Html/Noticias/Noticia2.html"
     },
     {
-        imagem: "../Imagens/noticia3.jpg",
+        imagem: "../../Imagens/noticia3.jpg",
         titulo: "Onde mora a esperança:",
         descricao: "Exposiçao convida a refletir sobre a potencia transformadora da moradia digna",
-        link: "../..//Html/Noticias/Noticia3.html" // Link adicionado
+        link: "../Html/Noticias/Noticia3.html"
     },
     {
-        imagem: "../Imagens/noticia4.png",
+        imagem: "../../Imagens/noticia4.png",
         titulo: "Habitat para a Humanidade revela: Moradia digna transforma saúde das mulheres em comunidades carentes",
         descricao: "Novo relatório da Habitat para a Humanidade vincula melhorias habitacionais em comunidades e favelas urbanas a benefícios importantes para a saúde das mulheres",
-        link: "../..//Html/Noticias/Noticia4.html" // Link adicionado
+        link: "../Html/Noticias/Noticia4.html"
     },
     {
-        imagem: "../Imagens/noticia 5.jpg",
+        imagem: "../../Imagens/noticia5.jpg", // Removido espaço no nome do arquivo
         titulo: "Serviços de consultoria pedagógica especializada",
         descricao: "Contratação de consultoria pedagógica para oficinas de saneamento em 33 escolas do Marajó, com capacitação de educadores e materiais didáticos.",
-        link: "../..//Html/Noticias/Noticia5.html" // Link adicionado
+        link: "../Html/Noticias/Noticia5.html"
     },
     {
-        imagem: "../Imagens/noticia6.jpg",
+        imagem: "../../Imagens/noticia6.jpg",
         titulo: "Reconstruindo Vidas: A Restauração de Moradias no RS Pós-Tragédia Climática",
         descricao: "A restauração de moradias no Rio Grande do Sul após enchentes e tragédia climática",
-        link: "../..//Html/Noticias/Noticia6.html" // Link adicionado
+        link: "../Html/Noticias/Noticia6.html"
     },
 ];
 
@@ -46,12 +45,10 @@ function criarNoticias() {
         const noticiaItem = document.createElement('div');
         noticiaItem.className = 'noticia-item';
 
-        // Criação da imagem se existir no objeto noticia
         let imagemHTML = '';
         if (noticia.imagem) {
-            imagemHTML = `<img src="${noticia.imagem}" alt="${noticia.titulo}" class="noticia-imagem" onerror="this.src='imagens/placeholder.jpg'">`;
+            imagemHTML = `<img src="${noticia.imagem}" alt="${noticia.titulo}" class="noticia-imagem" onerror="this.src='../../Imagens/placeholder.jpg'">`;
         } else {
-            // Imagem padrão caso não tenha imagem definida
             imagemHTML = `<div class="noticia-sem-imagem"></div>`;
         }
 
@@ -72,5 +69,4 @@ function criarNoticias() {
     });
 }
 
-// Chamada da função quando a página carregar
 window.onload = criarNoticias;
